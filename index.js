e4467f0590499d71abd009940b1ab4cf467ccd20
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-06-18 17:53:47 -0400

    commit tree 5dbb976ba6f6c8bcaf44be55cd82000944f7243d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let b = document.getElementsByTagName('body')[0];
+let child = b.getElementsByTagName('main')[0];
+b.removeChild(child);
+let newHeader = document.createElement('h1');
+newHeader.textContent = "Aaron is the champion";
+newHeader.setAttribute('id','victory');
+b.append(newHeader);
